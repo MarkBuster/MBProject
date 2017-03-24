3df82044a4268f3e772dcd6a08342f10ff69a580
yindongbo <1965634021@qq.com>
2017-03-24 16:54:38 +0800

    Initial Commit

---
+++ MBProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBProject
+//
+//  Created by yindongbo on 17/3/24.
+//  Copyright © 2017年 Dombo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBProject
+//
+//  Created by yindongbo on 17/3/24.
+//  Copyright © 2017年 Dombo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
